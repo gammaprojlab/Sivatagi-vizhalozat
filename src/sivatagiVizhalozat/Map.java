ffec9134432d7d081b89558b747e21a33e6fe73a
FGabi01 <46404350+FGabi01@users.noreply.github.com>
2023-05-08 16:08:14 +0200

    Created Map.java 2689 byte

---
+++ src/sivatagiVizhalozat/Map.java
@@ -0,0 +1,107 @@
+package sivatagiVizhalozat;
+
+import java.util.ArrayList;
+
+/**
+ * Stores the different fields and returns a fieldelement by a given type and id
+ */
+public class Map extends Serializable {
+    /**
+     * Stores cisterns on the map
+     */
+    private ArrayList<Cistern> cisterns;
+
+    /**
+     * Stores pipes on the map
+     */
+    private ArrayList<Pipe> pipes;
+
+    /**
+     * Stores pumps on the map
+     */
+    private ArrayList<Pump> pumps;
+
+    /**
+     * Stores springs on the map
+     */
+    private ArrayList<Spring> springs;
+
+    public Map() {
+        cisterns = new ArrayList<>();
+        pipes = new ArrayList<>();
+        springs = new ArrayList<>();
+        pumps = new ArrayList<>();
+    }
+
+    /**
+     * Returns a Cistern object by id
+     * @param id The id of the wanted Cistern object
+     * @return The Cistern object
+     */
+    public Cistern getCistern(int id) {
+        for (Cistern cistern : cisterns) {
+            if(Cistern.getId() == id) {
+                return cistern;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a Pipe object by id
+     * @param id The id of the wanted Pipe object
+     * @return The Pipe object
+     */
+    public Pipe getPipe(int id) {
+        for (Pipe pipe : pipes) {
+            if(pipe.getId() == id) {
+                return pipe;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a Pump object by id
+     * @param id The id of the wanted Pump object
+     * @return The Pump object
+     */
+    public Pump getPump(int id) {
+        for (Pump pump : pumps) {
+            if(pump.getId() == id) {
+                return pump;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a Spring object by id
+     * @param id The id of the wanted Spring object
+     * @return The Spring object
+     */
+    public Spring getSpring(int id) {
+        for (Spring spring : Springs) {
+            if(spring.getId() == id) {
+                return spring;
+            }
+        }
+        return null;
+    }
+    
+    /**
+    * Adds a new field element to the corresponding list 
+    * @param type The type of the object we want to add to the Map
+    * @param field The object we want to add to the Map
+    */
+    public void addFieldElement(string type, FieldElement field) {
+        switch (type) {
+            case "Pipe": if(!pipes.contains(field)) pipes.add(field); break;
+            case "Pump": if(!pumps.contains(field)) pumps.add(field); break;
+            case "Spring": if(!springs.contains(field)) springs.add(field); break;
+            case "Cistern": if(!cisterns.contains(field)) cisterns.add(field); break;
+            default: return;
+        }
+    }
+
+}
