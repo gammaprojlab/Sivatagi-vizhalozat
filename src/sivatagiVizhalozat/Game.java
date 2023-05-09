e586eac60dfc6b9713e3aea691a104d92f692f7f
Danee2023 <128070906+Danee2023@users.noreply.github.com>
2023-05-09 21:26:20 +0200

    Pump

--- src/sivatagiVizhalozat/Game.java
+++ src/sivatagiVizhalozat/Game.java
@@ -23,13 +23,48 @@ public class Game {
 	 * Saboteurs' points, as many spilled into the desert.
 	 * */
 	private int spilledWater;
+
+	/**
+	 * Points of the Plumbers.
+	 *  */
+	private int collectedWater;
+
+	/**
+	 * The mode of the current Game.
+	 * If the value is -1, then the game will take all random values to be truly random.
+	 * If the value is 0, then the game will take all random values to be 0.
+	 * If the value is 1, then the game will take all random values to be 1.
+	 *  */
+	private int tester;
 	
+
+	private ArrayList<int> classIdArray;
+
+	/**
+	 * Saboteurs who play the game.
+	 *  */
+	private ArrayList<Saboteur> saboteurs;
+
+	/**
+	 * Plumbers who play the game.
+	 *  */
+	private ArrayList<Plumber> plumbers;
+	
+	/////////////////////////////////////////////////////////////
+	/**
+	 * Fields of the map.
+	 *  */
+	private ArrayList<Steppable> steppable;
+	//////////////////////////////////////////////////////////////
+	
+
+
+
 	/**
 	 * Sets the Point of the Saboteurs.
 	 * @param s Points to what the Saboteurs point will be set.
 	 *  */
 	public void SetspilledWater(int s) {
-		Skeleton.Println(this.toString()+"SetspilledWater("+ int.class.getSimpleName() + " " + s +")");
 		spilledWater = s;
 	}
 	
@@ -38,21 +73,14 @@ public class Game {
 	 * @return spilledWater Points of the Saboteurs.
 	 *  */
 	public int GetspilledWater() {
-		Skeleton.Println(this.toString()+"GetspilledWater()");
 		return spilledWater;
 	}
-	
-	/**
-	 * Points of the Plumbers.
-	 *  */
-	private int collectedWater;
 	
 	/**
 	 * Sets the Point of the Plumbers.
 	 * @param c Points to what the Plumbers point will be set.
 	 *  */
 	public void SetcollectedWater(int c) {
-		Skeleton.Println(this.toString()+"SetcollectedWater("+ int.class.getSimpleName() + " " + c +")");
 		collectedWater = c;
 	}
 	
@@ -61,21 +89,14 @@ public class Game {
 	 * @return collectedWater Points of the Plumbers.
 	 *  */
 	public int GetcollectedWater() {
-		Skeleton.Println(this.toString()+"GetcollectedWater()");
 		return collectedWater;
 	}
-	
-	/**
-	 * Saboteurs who play the game.
-	 *  */
-	private ArrayList<Saboteur> saboteurs;
 	
 	/**
 	 * Adds a Saboteur to the game.
 	 * @param s Saboteur who will be playing.
 	 *  */
 	public void AddSaboteur(Saboteur s) {
-		Skeleton.Println(this.toString()+"AddSaboteur("+ Saboteur.class.getSimpleName() + " " + s +")");
 		saboteurs.add(s);
 	}
 	
@@ -84,21 +105,15 @@ public class Game {
 	 * @return saboteurs Saboteurs who play.
 	 *  */
 	public ArrayList<Saboteur> GetSaboteurs() {
-		Skeleton.Println(this.toString()+"GetSabotuer()");
 		return saboteurs;
 	}
 
-	/**
-	 * Plumbers who play the game.
-	 *  */
-	private ArrayList<Plumber> plumbers;
-	
+
 	/** 
 	 * Adds a Plumber to the game.
 	 * @param p Plumber who will be playing.
 	 * */
 	public void AddPlumber(Plumber p) {
-		Skeleton.Println(this.toString()+"AddPlumber("+ Plumber.class.getSimpleName() + " " + p +")");
 		plumbers.add(p);
 	}
 	
@@ -107,21 +122,14 @@ public class Game {
 	 * @return plumbers Plumbers who play.
 	 *  */
 	public ArrayList<Plumber> GetPlumbers() {
-		Skeleton.Println(this.toString()+"GetPlumber()");
 		return plumbers;
 	}
-	
-	/**
-	 * Fields of the map.
-	 *  */
-	private ArrayList<Steppable> steppable;
 	
 	/**
 	 * Adds to the Fields.
 	 * @param s New tiel to the map.
 	 *  */
 	public void AddSteppable(Steppable s) {
-		Skeleton.Println(this.toString()+"AddSteppable("+ Steppable.class.getSimpleName() + " " + s +")");
 		steppable.add(s);
 	}
 	
@@ -130,7 +138,6 @@ public class Game {
 	 * @return steppable Fields of the map.
 	 *  */
 	public ArrayList<Steppable> GetSteppable() {
-		Skeleton.Println(this.toString()+"GetSteppable()");
 		return steppable;
 	}
 	
@@ -138,7 +145,6 @@ public class Game {
 	 * Konstruktor of the Game class.
 	 *  */
 	public Game() {
-		Skeleton.Println(this.toString()+"Game()");
 		spilledWater = 0;
 		collectedWater = 0;
 		saboteurs = new ArrayList<Saboteur>();
@@ -150,17 +156,12 @@ public class Game {
 	 * Imitates the flow of the water.
 	 *  */
 	public void Tick() {
-		Skeleton.Println(this.toString()+"Tick()");
-		Skeleton.indentation++;
-
 		for(Steppable step: steppable) {
 			step.Step1();
 		}
 		for(Steppable step: steppable) {
 			step.Step2();
 		}
-
-		Skeleton.indentation--;
 	}
 	
 	/**
@@ -168,7 +169,6 @@ public class Game {
 	 * @param water The points what will be added.
 	 *  */
 	public void WaterSpilled(int water) {
-		Skeleton.Println(this.toString()+"WaterSpilled("+ int.class.getSimpleName() + " " + water +")");
 		spilledWater += water;
 	}
 	
@@ -177,7 +177,6 @@ public class Game {
 	 * @param water The points what will be added.
 	 *  */
 	public void WaterCollected(int water) {
-		Skeleton.Println(this.toString()+"WaterCollected("+ int.class.getSimpleName() + " " + water +")");
 		collectedWater += water;
 	}
 	
@@ -185,6 +184,5 @@ public class Game {
 	* Used for testing
 	*/
 	public String toString() {
-		return this.getClass().getSimpleName() + "'" + Integer.toHexString(this.hashCode()) + "'" + ".";
 	}
 }
