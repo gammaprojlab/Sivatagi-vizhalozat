1097cc97264b1ec031458adcfb5046dbafae1cf8
u3dk7x <bognarcsani@gmail.com>
2023-05-28 18:36:58 +0200

    Settings update

--- src/sivatagiVizhalozat/UIController.java
+++ src/sivatagiVizhalozat/UIController.java
@@ -2,7 +2,16 @@ package sivatagiVizhalozat;
 
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.swing.*;
 
@@ -55,46 +64,46 @@ public class UIController extends JFrame{
 	}
 	
 	public JPanel MainMenu() {
-		 JPanel mainPanel = new JPanel(new BorderLayout());
-	        mainPanel.setBackground(new Color(173, 216, 230));
+		JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(new Color(173, 216, 230));
 
-	        // Create a JPanel with BoxLayout for vertical alignment
-	        JPanel buttonPanel = new JPanel();
-	        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-	        buttonPanel.setBackground(new Color(173, 216, 230));
+        // Create a JPanel with BoxLayout for vertical alignment
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.setBackground(new Color(173, 216, 230));
 
-	        // Create and customize the buttons
-	        JButton startButton = createButton("Start", Color.GREEN, new Dimension(150, 30));
-	        startButton.addActionListener(action -> ChangePane(StartGame()));
-	        JButton settingsButton = createButton("Settings", Color.YELLOW, new Dimension(150, 30));
-	        settingsButton.addActionListener(action -> ChangePane((SettingsMenu())));
-	        JButton loadGameButton = createButton("LoadGame", Color.GRAY, new Dimension(150, 30));
-	        loadGameButton.addActionListener(action -> ChangePane(LoadGame()));
-	        JButton exitButton = createButton("Exit", Color.RED, new Dimension(150, 30));
-	        exitButton.addActionListener(action -> System.exit(0));
+        // Create and customize the buttons
+        JButton startButton = createButton("Start", Color.GREEN, new Dimension(150, 30));
+        startButton.addActionListener(action -> ChangePane(StartGame()));
+        JButton settingsButton = createButton("Settings", Color.YELLOW, new Dimension(150, 30));
+        settingsButton.addActionListener(action -> ChangePane((SettingsMenu())));
+        JButton loadGameButton = createButton("LoadGame", Color.GRAY, new Dimension(150, 30));
+        loadGameButton.addActionListener(action -> ChangePane(LoadGame()));
+        JButton exitButton = createButton("Exit", Color.RED, new Dimension(150, 30));
+        exitButton.addActionListener(action -> System.exit(0));
 
-	        // Add the buttons to the button panel with vertical spacing
-	        buttonPanel.add(Box.createVerticalGlue()); // Aligns buttons to the middle of the panel
-	        buttonPanel.add(startButton);
-	        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Adds vertical spacing
-	        buttonPanel.add(settingsButton);
-	        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-	        buttonPanel.add(loadGameButton);
-	        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-	        buttonPanel.add(exitButton);
-	        buttonPanel.add(Box.createVerticalGlue()); // Aligns buttons to the middle of the panel
+        // Add the buttons to the button panel with vertical spacing
+        buttonPanel.add(Box.createVerticalGlue()); // Aligns buttons to the middle of the panel
+        buttonPanel.add(startButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Adds vertical spacing
+        buttonPanel.add(settingsButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        buttonPanel.add(loadGameButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        buttonPanel.add(exitButton);
+        buttonPanel.add(Box.createVerticalGlue()); // Aligns buttons to the middle of the panel
 
-	        // Create a horizontal BoxLayout for horizontal alignment
-	        JPanel horizontalPanel = new JPanel();
-	        horizontalPanel.setLayout(new BoxLayout(horizontalPanel, BoxLayout.X_AXIS));
-	        horizontalPanel.add(Box.createHorizontalGlue()); // Aligns buttons to the middle of the panel
-	        horizontalPanel.add(buttonPanel);
-	        horizontalPanel.add(Box.createHorizontalGlue()); // Aligns buttons to the middle of the panel
-	        horizontalPanel.setBackground(new Color(173, 216, 230));
+        // Create a horizontal BoxLayout for horizontal alignment
+        JPanel horizontalPanel = new JPanel();
+        horizontalPanel.setLayout(new BoxLayout(horizontalPanel, BoxLayout.X_AXIS));
+        horizontalPanel.add(Box.createHorizontalGlue()); // Aligns buttons to the middle of the panel
+        horizontalPanel.add(buttonPanel);
+        horizontalPanel.add(Box.createHorizontalGlue()); // Aligns buttons to the middle of the panel
+        horizontalPanel.setBackground(new Color(173, 216, 230));
 
-	        // Add the horizontal panel to the center of the main panel
-	        mainPanel.add(horizontalPanel, BorderLayout.CENTER);
-	        return mainPanel;
+        // Add the horizontal panel to the center of the main panel
+        mainPanel.add(horizontalPanel, BorderLayout.CENTER);
+        return mainPanel;
 	}
 	
 	public JPanel StartGame() {
@@ -102,7 +111,65 @@ public class UIController extends JFrame{
 	}
 	
 	public JPanel SettingsMenu() {
-		return null;
+		JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(new Color(173, 216, 230));
+
+        // Create a JPanel with BoxLayout for vertical alignment
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.setBackground(new Color(173, 216, 230));
+
+        JLabel playerLabel = new JLabel("Players:");
+        JLabel mapLabel = new JLabel("Map:");
+        // Create an array of numbers from 1 to 8
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        // Create a JComboBox with the array of numbers
+        JComboBox<Integer> comboBox1 = new JComboBox<>(numbers);
+        String[] maps = (String[]) GetFiles("maps/").toArray();
+        JComboBox<String> comboBox2 = new JComboBox<>(maps);
+        comboBox1.setPreferredSize(new Dimension(150, 30));
+        comboBox1.setMaximumSize(new Dimension(150, 30));
+        comboBox2.setPreferredSize(new Dimension(150, 30));
+        comboBox2.setMaximumSize(new Dimension(150, 30));
+
+        // Add ActionListener to handle selection events
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Selected: ");
+            }
+        });
+        
+        comboBox2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Selected: ");
+            }
+        });
+        
+        // Add the buttons to the button panel with vertical spacing
+        buttonPanel.add(Box.createVerticalGlue()); // Aligns buttons to the middle of the panel
+        buttonPanel.add(playerLabel);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Adds vertical spacing
+        buttonPanel.add(comboBox1);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        buttonPanel.add(mapLabel);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        buttonPanel.add(comboBox2);
+        buttonPanel.add(Box.createVerticalGlue()); // Aligns buttons to the middle of the panel
+
+        // Create a horizontal BoxLayout for horizontal alignment
+        JPanel horizontalPanel = new JPanel();
+        horizontalPanel.setLayout(new BoxLayout(horizontalPanel, BoxLayout.X_AXIS));
+        horizontalPanel.add(Box.createHorizontalGlue()); // Aligns buttons to the middle of the panel
+        horizontalPanel.add(buttonPanel);
+        horizontalPanel.add(Box.createHorizontalGlue()); // Aligns buttons to the middle of the panel
+        horizontalPanel.setBackground(new Color(173, 216, 230));
+
+        // Add the horizontal panel to the center of the main panel
+        mainPanel.add(horizontalPanel, BorderLayout.CENTER);
+        return mainPanel;
 	}
 	
 	public JPanel LoadGame() {
@@ -120,5 +187,21 @@ public class UIController extends JFrame{
 		getContentPane().doLayout();
 		getContentPane().revalidate();
 		getContentPane().repaint();
+	}
+	
+	public List<String> GetFiles(String dir){
+
+		try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+
+			return stream
+					.filter(file -> !Files.isDirectory(file))
+					.map(Path::getFileName)
+					.map(Path::toString)
+					.collect(Collectors.toList());
+		}
+		catch(IOException e) {
+			System.out.println(e);
+		}
+		return null;
 	}
 }
